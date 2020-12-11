dc00f74766a771823af483df87cb7146e87afbc8
eungikimme <eungikim.me@gmail.com>
2020-12-11 10:30:20 +0900

    Add new mvp module and copy resoucres

---
+++ NewGitHubRepos/app-mvp/src/main/java/kr/eungi/newgithubrepos/RepositoryListActivity.java
@@ -0,0 +1,14 @@
+package kr.eungi.newgithubrepos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RepositoryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository_list);
+    }
+}
