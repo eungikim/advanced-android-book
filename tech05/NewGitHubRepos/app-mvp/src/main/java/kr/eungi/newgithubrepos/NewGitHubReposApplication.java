757d4da376c85f2e7a2a82f683fad70df072dd44
eungikimme <eungikim.me@gmail.com>
2020-12-15 11:42:39 +0900

    Change rx adapter dependency
    Use official library.

--- tech05/NewGitHubRepos/app-mvp/src/main/java/kr/eungi/newgithubrepos/NewGitHubReposApplication.java
+++ tech05/NewGitHubRepos/app-mvp/src/main/java/kr/eungi/newgithubrepos/NewGitHubReposApplication.java
@@ -5,11 +5,11 @@ import android.util.Log;
 
 import org.jetbrains.annotations.NotNull;
 
-import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
 import kr.eungi.newgithubrepos.model.GitHubService;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewGitHubReposApplication extends Application {
--- tech05/NewGitHubRepos/app-original/src/main/java/kr/eungi/newgithubrepos/NewGitHubReposApplication.java
+++ tech05/NewGitHubRepos/app-original/src/main/java/kr/eungi/newgithubrepos/NewGitHubReposApplication.java
@@ -5,10 +5,10 @@ import android.util.Log;
 
 import org.jetbrains.annotations.NotNull;
 
-import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewGitHubReposApplication extends Application {
