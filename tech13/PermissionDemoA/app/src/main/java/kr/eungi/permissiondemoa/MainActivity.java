d8e9bb835a6d307daab664137d3ec75b63a12049
eungikim <eungikim.me@gmail.com>
2021-01-05 23:03:18 +0900

    Add button for start app B's activity c

--- tech13/PermissionDemoA/app/src/main/java/kr/eungi/permissiondemoa/MainActivity.java
+++ tech13/PermissionDemoA/app/src/main/java/kr/eungi/permissiondemoa/MainActivity.java
@@ -30,5 +30,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(
+                            "permissiondemob.ACTION_LAUNCH_APP_B_ACTIVITY_C"));
+                } catch (ActivityNotFoundException anfe) {
+                    Toast.makeText(
+                            MainActivity.this,
+                            "PermissionDemoB 앱을 찾을 수 없습니다.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
