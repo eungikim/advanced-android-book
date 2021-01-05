e165fec43b2c858316cc0729c6be73a1ff489a45
eungikim <eungikim.me@gmail.com>
2021-01-05 23:04:13 +0900

    Add activity b and c

--- tech13/PermissionDemoB/app/src/main/java/kr/eungi/permissiondemob/ActivityC.java
+++ tech13/PermissionDemoB/app/src/main/java/kr/eungi/permissiondemob/ActivityC.java
@@ -6,13 +6,13 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ActivityB extends AppCompatActivity {
+public class ActivityC extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_b);
+        setContentView(R.layout.activity_c);
 
-        Toast.makeText(getApplicationContext(), "B 시작!!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "C 시작!!", Toast.LENGTH_SHORT).show();
     }
 }
